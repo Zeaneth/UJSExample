92051dad8600267c71d9fddd9eba34f4e831de20
Zamiz Tobar <zamiztobar@gmail.com>
2019-08-17 19:16:29 -0400

    Add simple form and UserFilter model

---
+++ app/models/user_filter.rb
@@ -0,0 +1,5 @@
+class UserFilter
+  include ActiveModel::Model
+
+  attr_accessor :name, :last_name, :age, :weight
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create([{name: "Star", last_name: "Wars", age: "25", weight:"80"},
+             {name: "Lord", last_name: "of the Rings", age: "205", weight:"200"}])
